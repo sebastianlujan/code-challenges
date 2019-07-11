c304d8af4842cc484f4d88b61ab8042b26025e4e
Sebastian Lujan <glujan.recalde@gmail.com>
2019-07-11 07:20:27 -0300

    no message

--- freecodecamp-js/test.js
+++ freecodecamp-js/test.js
@@ -1,22 +1,50 @@
 //test cases for red green refactor 
 
 /* 
-convertToF(0) should return a number
-Passed
-convertToF(-30) should return a value of -22
-Passed
-convertToF(-10) should return a value of 14
-Passed
-convertToF(0) should return a value of 32
-Passed
-convertToF(20) should return a value of 68
-Passed
-convertToF(30) should return a value of 86
+    convertToF(0) should return a number
+    Passed
+    convertToF(-30) should return a value of -22
+    Passed
+    convertToF(-10) should return a value of 14
+    Passed
+    convertToF(0) should return a value of 32
+    Passed
+    convertToF(20) should return a value of 68
+    Passed
+    convertToF(30) should return a value of 86
 */
 
 /*
-reverseString("hello") should return a string.
-reverseString("hello") should become "olleh".
-reverseString("Howdy") should become "ydwoH".
-reverseString("Greetings from Earth") should return "htraE morf sgniteerG".
+    reverseString("hello") should return a string.
+    reverseString("hello") should become "olleh".
+    reverseString("Howdy") should become "ydwoH".
+    reverseString("Greetings from Earth") should return "htraE morf sgniteerG".
+*/
+/*
+    factorialize(5) should return a number.
+    factorialize(5) should return 120.
+    factorialize(10) should return 3628800.
+    factorialize(20) should return 2432902008176640000.
+    factorialize(0) should return 1.
+*/
+
+/*
+findLongestWordLength("The quick brown fox jumped over the lazy dog") should return a number.
+Passed
+findLongestWordLength("The quick brown fox jumped over the lazy dog") should return 6.
+Passed
+findLongestWordLength("May the force be with you") should return 5.
+Passed
+findLongestWordLength("Google do a barrel roll") should return 6.
+Passed
+findLongestWordLength("What is the average airspeed velocity of an unladen swallow") should return 8.
+Passed
+findLongestWordLength("What if we try a super-long word such as otorhinolaryngology") should return 19.
+
+*/
+/*
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return an array.
+largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return [27, 5, 39, 1001].
+largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]) should return [9, 35, 97, 1000000].
+largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]) should return [25, 48, 21, -3].
 */
