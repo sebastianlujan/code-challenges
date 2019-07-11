7648ede531ada753ca7a6e6284fb039086d38aff
Sebastian Lujan <glujan.recalde@gmail.com>
2019-07-11 07:14:38 -0300

    findLongestWordLength

--- freecodecamp-js/solutions.js
+++ freecodecamp-js/solutions.js
@@ -68,5 +68,49 @@ factorialize = memoize(
     }
 );
 
-a = factorialize(5);
+//Return the length of the longest word in the provided sentence.
+//functional solution
+function findLongestWordLength(str) {
+    let max = 0;
+    str = str.split(' ');
+    str.forEach((x) => {
+        if(x.length >= max){
+            max = x.length;
+        }
+    });
+    return max;
+  }
+  
+//naive solution with for loop
+function findLongestWordLength(str) {
+    str = str.split(' ');
+    let max = 0;
+    for(let i = 0; i < str.length; i++){
+        if(str[i].length >= max){
+            max = str[i].length;
+        }
+    }
+    return max;
+}
+//a = findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+
+//Return an array consisting of the largest number from each provided sub-array. 
+//For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+
+function largestOfFour(arr) {
+ /*
+    let large = [];
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr.length; j++){
+            if            
+    }
+    return arr;
+*/  
+}
+  
+  a = largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
 console.log(a);
